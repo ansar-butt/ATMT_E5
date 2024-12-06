b9b124ca6f724567bb75b1b2a57b1720e760095a
ansar-butt <110402422+ansar-butt@users.noreply.github.com>
2024-12-06 02:04:53 +0100

    Commit

---
+++ translate_loop_script.sh
@@ -0,0 +1,5 @@
+for ((i = 3 ; i < 25 ; i = i + 3 )); do 
+    python translate_beam.py --data data/en-fr/prepared --dicts data/en-fr/prepared --checkpoint-path assignments/03/baseline/checkpoints/checkpoint_best.pt --output assignments/03/baseline/translations.txt --beam-size $i
+    bash scripts/postprocess.sh assignments/03/baseline/translations.txt assignments/03/baseline/translations.p.txt en
+    cat assignments/03/baseline/translations.p.txt | sacrebleu data/en-fr/raw/test.en > scores/score_$i.txt
+done
